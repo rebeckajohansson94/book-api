2ac40d160f123ea8128b48effce10c04d2015f2a
Rebecka Johansson <rebeckaemmajohansson@gmail.com>
2026-05-04 15:37:38 +0200

    First commit: setup project with express and dotenv, basic server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Could not find page" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
